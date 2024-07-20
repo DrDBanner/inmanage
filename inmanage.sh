e1f729e52c2b8b4d64d19ef05800fe47982a46ec
dros <github@private.r-system.de>
2024-07-20 21:55:59 +0200

    Initial commit

---
+++ inmanage.sh
@@ -0,0 +1,374 @@
+#!/bin/bash
+set -e
+
+# Function to prompt for user input
+prompt() {
+    local var_name="$1"
+    local default_value="$2"
+    local prompt_message="$3"
+
+    while true; do
+        read -r -p "${prompt_message} [default: $default_value]: " input
+        if [ -z "$input" ]; then
+            input="$default_value"
+        fi
+        if [ -n "$input" ]; then
+            echo "$input"
+            return
+        else
+            echo "Invalid input. Please try again."
+        fi
+    done
+}
+
+if [ ! -f ".inmanage/.env.inmanage" ]; then
+    echo ".inmanage/.env.inmanage configuration file for this script not found. Creating and configuring..."
+
+    # Query for configuration
+    echo -e "\n\n Just press [ENTER] to accept defaults. \n\n"
+    INM_BASE_DIRECTORY=$(prompt "INM_BASE_DIRECTORY" "$PWD/" "Which directory contains your IN installation folder? Must have a trailing slash.")
+    INM_INSTALLATION_DIRECTORY=$(prompt "INM_INSTALLATION_DIRECTORY" "./invoiceninja" "What is the installation directory name? Must be relative from $INM_BASE_DIRECTORY and can start with a . dot.")
+    INM_ENV_FILE="$INM_BASE_DIRECTORY$INM_INSTALLATION_DIRECTORY/.env"
+    INM_TEMP_DOWNLOAD_DIRECTORY="./._in_tempDownload"
+    INM_BACKUP_DIRECTORY=$(prompt "INM_BACKUP_DIRECTORY" "./_in_backups" "Where shall backups go?")
+    INM_FORCE_READ_DB_PW=$(prompt "INM_FORCE_READ_DB_PW" "N" "Include database password in backup command? If Y we read it from Invoice Ninja installation, but it's a security concern and may be visible for other server users while the task is running. If N the script assumes you have a secure and working .my.cnf file with your DB credentials. (Y/N)")
+    INM_ENFORCED_USER=$(prompt "INM_ENFORCED_USER" "web" "The user running the script? Should be the webserver user in most cases. Check twice if this value is set correct according to your webserver setup.")
+    INM_ENFORCED_SHELL=$(prompt "INM_ENFORCED_SHELL" "$(command -v bash)" "Which shell should be used? In doubt, keep as is.")
+    INM_PHP_EXECUTABLE=$(prompt "INM_PHP_EXECUTABLE" "$(command -v php)" "Path to the PHP executable? In doubt, keep as is.")
+    INM_ARTISAN_STRING="$INM_PHP_EXECUTABLE $INM_BASE_DIRECTORY$INM_INSTALLATION_DIRECTORY/artisan"
+    INM_PROGRAM_NAME="InvoiceNinja"
+    INM_COMPATIBILITY_VERSION="5+"
+    INM_KEEP_BACKUPS=$(prompt "INM_KEEP_BACKUPS" "2" "How many backup files and update iterations to keep? If you keep 7 and backup on a daily basis you have 7 snapshots available.")
+
+    # Save configuration to .env.inmanage
+    cat <<EOL >.inmanage/.env.inmanage
+INM_BASE_DIRECTORY="$INM_BASE_DIRECTORY"
+INM_INSTALLATION_DIRECTORY="$INM_INSTALLATION_DIRECTORY"
+INM_ENV_FILE="$INM_ENV_FILE"
+INM_TEMP_DOWNLOAD_DIRECTORY="$INM_TEMP_DOWNLOAD_DIRECTORY"
+INM_BACKUP_DIRECTORY="$INM_BACKUP_DIRECTORY"
+INM_ENFORCED_USER="$INM_ENFORCED_USER"
+INM_ENFORCED_SHELL="$INM_ENFORCED_SHELL"
+INM_PHP_EXECUTABLE="$INM_PHP_EXECUTABLE"
+INM_ARTISAN_STRING="$INM_ARTISAN_STRING"
+INM_PROGRAM_NAME="$INM_PROGRAM_NAME"
+INM_KEEP_BACKUPS="$INM_KEEP_BACKUPS"
+INM_FORCE_READ_DB_PW="$INM_FORCE_READ_DB_PW"
+EOL
+
+    echo ".env.inmanage has been created and configured."
+    target="$INM_BASE_DIRECTORY.inmanage/inmanage.sh"
+    link="$INM_BASE_DIRECTORY/inmanage.sh"
+
+    # Check if the link exists
+    if [ -L "$link" ]; then
+        # Check if it points to the correct target
+        if [ "$(readlink "$link")" == "$target" ]; then
+            echo "The symlink is correct."
+        else
+            echo "The symlink is incorrect. Updating..."
+            ln -sf "$target" "$link"
+        fi
+    else
+        echo "The symlink does not exist. Creating..."
+        ln -s "$target" "$link"
+    fi
+
+    echo "A symlink to this script has been created in the installation directory."
+fi
+
+source .inmanage/.env.inmanage
+
+# Check required commands
+check_commands() {
+    local commands=("curl" "tar" "cp" "mv" "mkdir" "chown" "find" "rm" "mysqldump" "grep" "xargs" "php" "read" "source")
+    for cmd in "${commands[@]}"; do
+        if ! command -v "$cmd" &>/dev/null; then
+            echo "Error: Command '$cmd' is not available. Please install it and try again."
+            exit 1
+        fi
+    done
+}
+
+# Ensure script runs as INM_ENFORCED_USER
+if [ "$(whoami)" != "$INM_ENFORCED_USER" ]; then
+    INM_SCRIPT_PATH=$(realpath "$0")
+    echo "Switching to user '$INM_ENFORCED_USER'."
+    exec sudo -u "$INM_ENFORCED_USER" "$INM_ENFORCED_SHELL" -c "cd '$(pwd)' && \"$INM_ENFORCED_SHELL\" \"$INM_SCRIPT_PATH\" \"$@\""
+    exit 0
+fi
+
+# Get installed version
+get_installed_version() {
+    local version
+    if [ -f "$INM_BASE_DIRECTORY$INM_INSTALLATION_DIRECTORY/VERSION.txt" ]; then
+        version=$(cat "$INM_BASE_DIRECTORY$INM_INSTALLATION_DIRECTORY/VERSION.txt") || {
+            echo "Failed to read installed version"
+            exit 1
+        }
+        echo "$version"
+    else
+        echo "VERSION.txt not found"
+        exit 1
+    fi
+}
+
+# Get latest version
+get_latest_version() {
+    local version
+    version=$(curl -s https://api.github.com/repos/invoiceninja/invoiceninja/releases/latest | grep tag_name | cut -d '"' -f 4 | sed 's/v//') || {
+        echo "Failed to retrieve latest version"
+        exit 1
+    }
+    echo "$version"
+}
+
+# Download Invoice Ninja
+download_ninja() {
+    [ -d "$INM_TEMP_DOWNLOAD_DIRECTORY" ] && rm -Rf "$INM_TEMP_DOWNLOAD_DIRECTORY"
+    mkdir -p "$INM_TEMP_DOWNLOAD_DIRECTORY" || {
+        echo "Failed to create temp directory"
+        exit 1
+    }
+    cd "$INM_TEMP_DOWNLOAD_DIRECTORY" || {
+        echo "Failed to change to temp directory"
+        exit 1
+    }
+
+    local app_version
+    app_version=$(get_latest_version)
+    sleep 2
+    echo "Downloading Invoice Ninja version $app_version..."
+    curl -sL "https://github.com/invoiceninja/invoiceninja/releases/download/v$app_version/invoiceninja.tar" -o invoiceninja.tar || {
+        echo "Failed to download"
+        exit 1
+    }
+}
+
+# Run update
+run_update() {
+    local installed_version latest_version
+
+    installed_version=$(get_installed_version)
+    latest_version=$(get_latest_version)
+
+    if [ "$installed_version" == "$latest_version" ] && [ "$force_update" != true ]; then
+        read -p "Already up-to-date. Proceed with update? (yes/no): " response
+        if [[ ! "$response" =~ ^[Yy]([Ee][Ss])?$ ]]; then
+            echo "Update aborted."
+            exit 0
+        fi
+    fi
+
+    echo "Update starts now."
+    download_ninja
+    mkdir "$INM_INSTALLATION_DIRECTORY" || {
+        echo "Failed to create installation directory"
+        exit 1
+    }
+    chown "$INM_ENFORCED_USER" "$INM_INSTALLATION_DIRECTORY" || {
+        echo "Failed to change owner"
+        exit 1
+    }
+
+    tar -xf invoiceninja.tar -C "$INM_INSTALLATION_DIRECTORY" || {
+        echo "Failed to unpack"
+        exit 1
+    }
+
+    $INM_ARTISAN_STRING down || {
+        echo "Failed to run artisan down"
+        exit 1
+    }
+    cp "$INM_ENV_FILE" "$INM_INSTALLATION_DIRECTORY/" || {
+        echo "Failed to copy .env"
+        exit 1
+    }
+    cp -R "$INM_BASE_DIRECTORY$INM_INSTALLATION_DIRECTORY/public/storage/." "$INM_INSTALLATION_DIRECTORY/public/storage/" || {
+        echo "Failed to copy storage"
+        exit 1
+    }
+
+    mv "$INM_BASE_DIRECTORY$INM_INSTALLATION_DIRECTORY" "$INM_BASE_DIRECTORY${INM_INSTALLATION_DIRECTORY}_$(date +'%Y%m%d_%H%M%S')" || {
+        echo "Failed to rename old installation"
+        exit 1
+    }
+    mv "$INM_BASE_DIRECTORY$INM_TEMP_DOWNLOAD_DIRECTORY/$INM_INSTALLATION_DIRECTORY" "$INM_BASE_DIRECTORY$INM_INSTALLATION_DIRECTORY" || {
+        echo "Failed to move new installation"
+        exit 1
+    }
+
+    $INM_ARTISAN_STRING up || {
+        echo "Failed to run artisan up"
+        exit 1
+    }
+    $INM_ARTISAN_STRING ninja:post-update || {
+        echo "Failed to run post-update"
+        exit 1
+    }
+    $INM_ARTISAN_STRING cache:clear || {
+        echo "Failed to clear cache"
+        exit 1
+    }
+    $INM_ARTISAN_STRING ninja:translations || {
+        echo "Failed to run translations"
+        exit 1
+    }
+
+    cleanup_old_versions
+}
+
+# Run backup
+run_backup() {
+    if [ ! -d "$INM_BASE_DIRECTORY$INM_BACKUP_DIRECTORY" ]; then
+        echo "Creating backup directory."
+        mkdir -p "$INM_BASE_DIRECTORY$INM_BACKUP_DIRECTORY" || {
+            echo "Failed to create backup directory"
+            exit 1
+        }
+    else
+        echo "Backup directory already exists. Using it."
+    fi
+
+    if [ -f "$INM_ENV_FILE" ]; then
+        local export_vars
+        export_vars=$("$INM_ENFORCED_SHELL" -c "grep '^DB_' '$INM_ENV_FILE' | xargs") || {
+            echo "Failed to extract DB variables"
+            exit 1
+        }
+        eval "$export_vars" || {
+            echo "Failed to evaluate DB variables"
+            exit 1
+        }
+
+        if [ -z "$DB_HOST" ] || [ -z "$DB_DATABASE" ] || [ -z "$DB_USERNAME" ] || [ -z "$DB_PORT" ]; then
+            echo "Some DB variables are missing."
+            exit 1
+        fi
+    fi
+
+    cd "$INM_BACKUP_DIRECTORY" || {
+        echo "Failed to change to backup directory"
+        exit 1
+    }
+
+    if [ "$INM_FORCE_READ_DB_PW" == "Y" ]; then
+        mysqldump -f --no-create-db -h "$DB_HOST" -u "$DB_USERNAME" -p"$DB_PASSWORD" "$DB_DATABASE" >"${DB_DATABASE}_$(date +'%Y%m%d_%H%M%S').sql" || {
+            echo "Failed to dump database"
+            exit 1
+        }
+    else
+        echo "Using .my.cnf for database access"
+        mysqldump -f --no-create-db -h "$DB_HOST" -u "$DB_USERNAME" "$DB_DATABASE" >"${DB_DATABASE}_$(date +'%Y%m%d_%H%M%S').sql" || {
+            echo "Failed to dump database"
+            exit 1
+        }
+    fi
+
+    cd "$INM_BASE_DIRECTORY" || {
+        echo "Failed to change to base directory"
+        exit 1
+    }
+    tar -czf "${INM_PROGRAM_NAME}_$(date +'%Y%m%d_%H%M%S').tar.gz" "$INM_BACKUP_DIRECTORY"/*.sql -C "$INM_INSTALLATION_DIRECTORY" . || {
+        echo "Failed to create backup"
+        exit 1
+    }
+    rm "$INM_BACKUP_DIRECTORY"/*.sql || {
+        echo "Failed to remove SQL files"
+        exit 1
+    }
+    mv ${INM_PROGRAM_NAME}_*.tar.gz "$INM_BACKUP_DIRECTORY" || {
+        echo "Failed to move backups"
+        exit 1
+    }
+
+    cleanup_old_backups
+}
+
+# Cleanup old versions
+cleanup_old_versions() {
+    echo "Cleaning up old update directory versions."
+    local update_dirs
+    update_dirs=$(find "$INM_BASE_DIRECTORY" -maxdepth 1 -type d -name "$(basename "$INM_INSTALLATION_DIRECTORY")_*" | sort -r | tail -n +$((INM_KEEP_BACKUPS + 1)))
+
+    if [ -n "$update_dirs" ]; then
+        echo "$update_dirs" | xargs -r rm -rf || {
+            echo "Failed to clean up old versions"
+            exit 1
+        }
+    fi
+
+    ls -la "$INM_BASE_DIRECTORY"
+}
+
+
+# Cleanup old backups
+cleanup_old_backups() {
+    echo "Cleaning up old backups."
+    
+    # Find backup files and list them
+    local backup_files
+    backup_files=$(find "$INM_BASE_DIRECTORY$INM_BACKUP_DIRECTORY" -maxdepth 1 -type f -name "*.tar.gz" | sort -r | tail -n +$((INM_KEEP_BACKUPS + 1)))
+
+    if [ -n "$backup_files" ]; then
+        echo "$backup_files" | xargs -r rm -f || {
+            echo "Failed to clean up old backups"
+            exit 1
+        }
+    fi
+
+    # List remaining files in the backup directory
+    ls -la "$INM_BASE_DIRECTORY$INM_BACKUP_DIRECTORY"
+}
+
+
+
+# Function caller
+function_caller() {
+    case "$1" in
+    update)
+        run_update
+        ;;
+    backup)
+        run_backup
+        ;;
+    cleanup_versions)
+        cleanup_old_versions
+        ;;
+    cleanup_backups)
+        cleanup_old_backups
+        ;;
+    *)
+        echo "Unknown parameter $1"
+        return 1
+        ;;
+    esac
+}
+
+# Parse command-line options
+force_update=false
+while [[ $# -gt 0 ]]; do
+    case $1 in
+    --force)
+        force_update=true
+        shift
+        ;;
+    update | backup | cleanup_versions | cleanup_backups)
+        command=$1
+        shift
+        ;;
+    *)
+        echo -e "\n\n Usage: ./inmanage.sh <update|backup|cleanup_versions|cleanup_backups> [--force] \n\n"
+        exit 1
+        ;;
+    esac
+done
+
+if [ -z "$command" ]; then
+    echo -e "\n\n Usage: ./inmanage.sh <update|backup|cleanup_versions|cleanup_backups> [--force] \n\n"
+    exit 1
+fi
+
+check_commands
+
+cd "$INM_BASE_DIRECTORY" && function_caller "$command"
---
+++ readme.md
@@ -0,0 +1,125 @@
+# Invoice Ninja Management Script
+
+Easily update and back up your self-hosted Invoice Ninja instance with a shell script.
+
+This script is designed to manage your Invoice Ninja installation starting from version 5 and above by performing updates, backups, and cleanup tasks. It uses a configuration file to set up necessary environment variables and ensure required commands are available. On the first start, it will help you set up the configuration.
+
+When you call the script, do it as a user who has the rights to read the `.env` file of your Invoice Ninja installation. Most likely, it's the webserver's user, such as `web`, `apache`, or `nginx`. For example:
+
+## Interactions
+
+### Installation
+
+Go to your base directory where the `invoiceninja` folder resides. Then run:
+
+```bash
+sudo -u web bash -c "git clone @dros/inmamage .inmanage && chmod +x .inmanage/inmanage.sh && bash .inmanage/inmanage.sh"
+```
+
+Ensure that "web" is the correct user (substitute if necessary) who has all the permissions to your Invoice Ninja installation, including reading the .env file.
+
+### Running the script
+
+If everything went well with your installation, you now have a symlink in your base directory from which you can call the script directly. For example:
+
+```bash
+sudo -u web bash -c "bash ./inmanage.sh backup"
+```
+
+This example shows how to run the script in a bash as user 'web'. This can be different on your machine. During the installation you have been asked which user shall execute this script. So, if you call it now and you are not that particular user, you'll get prompted to enter the password in order to switch to that user. So, you probably want to call it with the right user from the get go.
+
+### Update the script
+
+You can update the script with
+
+```bash
+cd .inmanage && sudo -u web git pull
+```
+
+Mind the user 'web' here as well.
+
+### Run as cronjob
+
+If you want to run it as a cronjob add this line to your crontab. Mind the user 'web' here as well.
+
+```bash
+0 2 * * * web /path/to/your/inmanage.sh backup > /path/to/logfile 2>&1
+```
+
+## Key Functions
+
+A little overview what's happening under the hood.
+
+1. **Configuration File Setup**:
+
+   - If `.env.inmanage` is not found, the script creates it and prompts for settings like installation directory, backup locations, and other configurations.
+
+2. **Environment Variables**:
+
+   - Loads values from `.env.inmanage` to configure paths, user settings, PHP executable, and other details.
+
+3. **Command Check**:
+
+   - Verifies that required commands (`curl`, `tar`, `php`, etc.) are installed and available on your system.
+
+4. **User Check**:
+
+   - Ensures the script is run under the correct user account to avoid permission issues.
+
+5. **Reads Invoice Ninja Configuration**
+
+- The script reads data from the IN .env file to determine the database connection in order to execute the mysqldump. By default it assumes you have a working .my.cnf file which holds the database credentials. If you have set `INM_FORCE_READ_DB_PW="Y"` in your configuration, then it will grab the password and pass it to the mysqldump command. Which CAN be a security issue. So, handle with care.
+
+## Commands
+
+- **`update`**:
+
+  - Downloads and installs the latest version of Invoice Ninja from Github.
+  - Updates the installation, copies environment files, and updates storage settings. With a `--force` switch you can force to re-run the update task even if you are on the most recent version.
+  - Executes cleanups by default.
+
+- **`backup`**:
+
+  - Creates backups of the database and files.
+  - Ensures the backup directory exists, performs the backup, and cleans up old backups.
+  - Executes cleanups by default.
+
+- **`cleanup_versions`**:
+
+  - Deletes old versions of the Invoice Ninja installation directories.
+  - Keeps only a specified number of recent versions defined in `INM_KEEP_BACKUPS` during installation.
+
+- **`cleanup_backups`**:
+  - Removes old backup files.
+  - Keeps only a specified number of recent backups defined in `INM_KEEP_BACKUPS` during installation.
+
+## Execution
+
+Run the script with one of the following commands to perform the associated tasks:
+
+```bash
+./inmanage.sh update
+./inmanage.sh backup
+./inmanage.sh cleanup_versions
+./inmanage.sh cleanup_backups
+```
+
+## Roadmap
+
+Maybe I'll add some more functionality like initial installation and sync to external locations. We'll see.
+
+## Contribs
+
+The beloved Invoice Ninja
+https://github.com/invoiceninja/invoiceninja
+
+### Donations
+
+If you feel you'd like to donate something for this script go for it:
+
+- **Bitcoin [BTC]** bc1qj3tpz90q3m9hyw8q6qgkdswgk68k34aktehr2h
+- **Bitcoin Cash [BCH]** 1DucLq4AJP5R53qMT9iRZnAveA17DQyCdp
+- **Tether ETH** 0xA4099E3783578c490975d12d5680F1Aa739DD5d1
+- **Tether SOL** G1RBqC7zZJSPQQ1gQ5DUSNksS3ZGFXHPYKfkqYN6eG36
+- **Doge [DOGE]** DM2LAxAyC4Ug7mBpaGAnYygerX8RtZdxom
+- **Tron [TRX]** TXkVPuKfTiaSz3mtMZ9NTqhEH6EW7bF3gC
