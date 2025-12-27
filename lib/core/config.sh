f3c36234c64d644f3c69b570985097d57099551e
dros <github@private.r-system.de>
2025-12-28 00:43:23 +0100

    config: clarify defaults and disable cache sudo prompt

--- lib/core/config.sh
+++ lib/core/config.sh
@@ -17,8 +17,8 @@ declare -A default_settings=(
     ["INM_ENV_FILE"]="\${INM_BASE_DIRECTORY}\${INM_INSTALLATION_DIRECTORY}/.env"
     ["INM_CACHE_LOCAL_DIRECTORY"]="./.cache"
     ["INM_CACHE_GLOBAL_DIRECTORY"]="\${HOME}/.inmanage/cache"
-    ["INM_CACHE_SUDO_PROMPT"]="ask"
-    ["INM_CACHE_GLOBAL_RETENTION"]="3"
+    ["INM_CACHE_SUDO_PROMPT"]="never" # ask|never to enable sudo prompt for cache dir.
+    ["INM_CACHE_GLOBAL_RETENTION"]="3" # Keep last N cached releases.
     ["INM_DUMP_OPTIONS"]="--default-character-set=utf8mb4 --no-tablespaces --skip-add-drop-table --quick --single-transaction"
     ["INM_BACKUP_DIRECTORY"]="./.backups"
     ["INM_FORCE_READ_DB_PW"]="N"
@@ -29,34 +29,31 @@ declare -A default_settings=(
     ["INM_PROGRAM_NAME"]="InvoiceNinja"
     ["INM_COMPATIBILITY_VERSION"]="5+"
     ["INM_KEEP_BACKUPS"]="2"
-    ["INM_GH_API_CREDENTIALS"]="" #format username:password or token:x-oauth.
-    ["INM_MIGRATION_BACKUP"]=""
+    ["INM_GH_API_CREDENTIALS"]="" # Format username:password or token:x-oauth.
+    ["INM_MIGRATION_BACKUP"]="" # Use LATEST or path to run restore after provision.
+    ["INM_CLI_COMPATIBILITY"]="new" # Missing => treat as legacy install.
 )
 
 # shellcheck disable=SC2034
 prompt_order=(
     "INM_BASE_DIRECTORY"
     "INM_INSTALLATION_DIRECTORY"
-    "INM_DUMP_OPTIONS"
-    "INM_BACKUP_DIRECTORY"
     "INM_KEEP_BACKUPS"
     "INM_FORCE_READ_DB_PW"
     "INM_ENFORCED_USER"
-    "INM_ENFORCED_SHELL"
-    "INM_PHP_EXECUTABLE"
     "INM_GH_API_CREDENTIALS"
 )
 
 # shellcheck disable=SC2034,SC2190
 declare -A prompt_texts=(
-    ["INM_BASE_DIRECTORY"]="Base directory location? This will contain your Invoice Ninja app directory (next step). Not the web docroot."
-    ["INM_INSTALLATION_DIRECTORY"]="Invoice Ninja app directory (relative to \$INM_BASE_DIRECTORY or absolute). The web server usually serves from <app>/public."
-    ["INM_DUMP_OPTIONS"]="Modify database dump options: In doubt, keep defaults."
-    ["INM_BACKUP_DIRECTORY"]="Backup Directory?"
-    ["INM_FORCE_READ_DB_PW"]="Include DB password in CLI? (Y): Convenient, but may expose the password to other server users during runtime. (N): Assumes a secure .my.cnf file with credentials to avoid exposure."
-    ["INM_ENFORCED_USER"]="Script user? Usually the webserver user. Ensure it matches your webserver setup."
-    ["INM_ENFORCED_SHELL"]="Which shell should be used? In doubt, keep as is."
-    ["INM_PHP_EXECUTABLE"]="Path to the PHP executable? In doubt, keep as is."
-    ["INM_KEEP_BACKUPS"]="Backup retention? Set to 2 for daily backups to keep 2 snapshots. Ensure enough disk space."
+    ["INM_BASE_DIRECTORY"]="BASE_DIRECTORY: This will contain your Invoice Ninja app directory (next step). It's not webserver's docroot. Define your desired location or keep."
+    ["INM_INSTALLATION_DIRECTORY"]="INSTALLATION_DIRECTORY: Invoice Ninja App directory. The web-server usually serves from <INSTALLATION_DIRECTORY>/public. Define your desired location or keep."
+    ["INM_DUMP_OPTIONS"]="DUMP_OPTIONS: Modify database dump options: In doubt, keep defaults."
+    ["INM_BACKUP_DIRECTORY"]="BACKUP_DIRECTORY: Define your desired location or keep."
+    ["INM_FORCE_READ_DB_PW"]="FORCE_READ_DB_PW: Include DB password in CLI? (Y): Convenient, but may expose the password to other server users during runtime. (N): Assumes a secure .my.cnf file with credentials to avoid exposure."
+    ["INM_ENFORCED_USER"]="ENFORCED_USER: Correct setting helps to mitigate permission issues. Usually the webserver user. On shared hosting often your current user. If current is true, you can leave this empty."
+    ["INM_ENFORCED_SHELL"]="ENFORCED_SHELL: In doubt, keep as is."
+    ["INM_PHP_EXECUTABLE"]="PHP_EXECUTABLE: In doubt, keep as is."
+    ["INM_KEEP_BACKUPS"]="KEEP_BACKUPS: Backup retention? Set to 2 to keep 2 backups in the past at a time. Ensure enough disk space and keep the backup frequency in mind."
     ["INM_GH_API_CREDENTIALS"]="GitHub API credentials may be required on shared hosting. Use the format username:password or token:x-oauth. If provided, all curl commands will use these credentials;"
 )
